5afd9b9f07e43009dc75238d9f0e129435877439
chupa <76084340+panichev-sa@users.noreply.github.com>
2025-12-12 15:18:06 +0700

    Update run.sh

--- torrserver/.build/rootfs/run.sh
+++ torrserver/.build/rootfs/run.sh
@@ -32,55 +32,29 @@ if [ ! -d $TS_TORR_DIR ]; then
   mkdir -p $TS_TORR_DIR
 fi
 
-# Build accs.db if httpauth enabled
-# if [[ "$(bashio::config 'httpauth')" = true ]]
-# then
-#   bashio::log.info "HTTPAuth: enabled"
-#   FLAGS="${FLAGS} --httpauth"
-#   ACCESS_DB="${TS_CONF_PATH}/accs.db"
-#   jq_args=(-n )
-#   for key in $(bashio::config "logins|keys"); do
-#     USERNAME=$(bashio::config "logins[${key}].username")
-#     PASSWORD=$(bashio::config "logins[${key}].password")
+Build accs.db if httpauth enabled
+if [[ "$(bashio::config 'httpauth')" = true ]]
+then
+  bashio::log.info "HTTPAuth: enabled"
+  FLAGS="${FLAGS} --httpauth"
+  ACCESS_DB="${TS_CONF_PATH}/accs.db"
+  jq_args=(-n )
+  for key in $(bashio::config "logins|keys"); do
+    USERNAME=$(bashio::config "logins[${key}].username")
+    PASSWORD=$(bashio::config "logins[${key}].password")
 
-#     # Warning if password is not safe
-#     if ! bashio::config.is_safe_password "${PASSWORD}"; then
-#       bashio::log.warning "Password for user '${USERNAME}' is not safe!"
-#     fi
+    # Warning if password is not safe
+    if ! bashio::config.is_safe_password "${PASSWORD}"; then
+      bashio::log.warning "Password for user '${USERNAME}' is not safe!"
+    fi
 
-#     jq_args+=( --arg "${USERNAME}" "${PASSWORD}" )
-#     bashio::log.info "HTTPAuth: Added '${USERNAME}' user"
-#   done
-#   jq "${jq_args[@]}" '$ARGS.named' > "$ACCESS_DB"
-# else
-#   bashio::log.notice "HTTPAuth: disabled"
-# fi
-
-# Create access db
-ACCESS_DB="${TS_CONF_PATH}/accs.db"
-jq_args=(-n )
-for key in $(bashio::config "logins|keys"); do
-  USERNAME=$(bashio::config "logins[${key}].username")
-  PASSWORD=$(bashio::config "logins[${key}].password")
-
-  # Warning if password is not safe
-  if ! bashio::config.is_safe_password "${PASSWORD}"; then
-    bashio::log.warning "Password for user '${USERNAME}' is not safe!"
-  fi
-
-  jq_args+=( --arg "${USERNAME}" "${PASSWORD}" )
-  bashio::log.info "HTTPAuth: Added '${USERNAME}' user"
-done
-jq "${jq_args[@]}" '$ARGS.named' > "$ACCESS_DB"
-
-# if httpauth enabled
-# if [[ "$(bashio::config 'httpauth')" = true ]]
-# then
-bashio::log.info "HTTPAuth: enabled"
-FLAGS="${FLAGS} --httpauth"
-# else
-#   bashio::log.notice "HTTPAuth: disabled"
-# fi
+    jq_args+=( --arg "${USERNAME}" "${PASSWORD}" )
+    bashio::log.info "HTTPAuth: Added '${USERNAME}' user"
+  done
+  jq "${jq_args[@]}" '$ARGS.named' > "$ACCESS_DB"
+else
+  bashio::log.notice "HTTPAuth: disabled"
+fi
 
 Add M3U_CUSTOM_HOST env
 if [[ "$(bashio::config 'm3u_custom_host')" ]]
